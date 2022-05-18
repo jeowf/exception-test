45b66241895da554e21ebe1c4cbcb61bbe283296
Felipe Morais <moraisfelipe97@gmail.com>
2022-05-18 17:21:47 -0300

    Padrão

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping("/throw")
+    public void raiseException() throws Exception{
+        LOGGER.info(">>>>> /throw");
+        throw new Exception("Exception!");
+    }
+
+    @GetMapping("/throw-log")
+    public void raiseExceptionWithLog() throws Exception{
+        LOGGER.info(">>>>> /throw-log");
+        Exception e = new Exception("Exception!");
+        LOGGER.error("logging", e);
+        throw e;
+    }
+
+}
